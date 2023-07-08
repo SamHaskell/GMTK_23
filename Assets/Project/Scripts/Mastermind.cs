c4e380e45eb664554d7fb0d6b01108009d710dcb
CatTortoise <eiyi6688@gmail.com>
2023-07-08 18:48:41 +0300

    Mastermind
    CheckResult()

--- Assets/Project/Scripts/Mastermind.cs
+++ Assets/Project/Scripts/Mastermind.cs
@@ -15,9 +15,13 @@ public class Mastermind {
     public List<ITagble.Tag> SolutionTags; // Tag list for the current game solution
     public List<ITagble.Tag> KnownTags; // Tag list for tags known to player on current guess
     public int TurnsLeft;
+    public Dictionary<Result, int> GuessResult;
+
 
     public Mastermind(SolutionData solution, int turns)
     {
+        this.KnownTags = new();
+        GuessHistory = new();
         this.SolutionTags = solution.Tags;
         foreach (ITagble.Tag tag in SolutionTags) {
             this.KnownTags.Add(ITagble.Tag.NONE);
@@ -39,6 +43,34 @@ public class Mastermind {
         }
         GuessHistory.Add(guess);
         TurnsLeft --;
+
+    }
+
+    public void CheckResult()
+    {
+        GuessResult = new Dictionary<Result, int> { {Result.CORRECT, 0 }, 
+                                                    { Result.PARTIAL, 0 }, 
+                                                    { Result.INCORRECT, 0 } 
+                                                    };
+        List<Result> LastGuess = GuessHistory.Last();
+
+        foreach (Result result in LastGuess)
+        {
+            switch (result)
+            {
+                case Result.INCORRECT:
+                    GuessResult[Result.INCORRECT] ++;
+                    break;
+                case Result.PARTIAL:
+                    GuessResult[Result.PARTIAL]++;
+                    break;
+                case Result.CORRECT:
+                    GuessResult[Result.CORRECT]++;
+                    break;
+            }
+        }
+
+ 
     }
 
 }
---
+++ Assets/Project/Scripts/TagPackage.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagPackage : MonoBehaviour,ITagble
+{
+    public TagData PackageTagData;
+
+    public ITagble.Tag GetTag()
+    {
+        return PackageTagData.GetTag();
+    }
+}
