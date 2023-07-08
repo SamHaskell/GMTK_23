af1df3098cdacb3a6713fbf7c5395b7e35122fba
SamHaskell <samuel.haskell@manchester.ac.uk>
2023-07-08 17:55:36 +0100

    commit sam

--- Assets/Project/Scripts/TagPackage.cs
+++ Assets/Project/Scripts/TagPackage.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TagPackage : MonoBehaviour,ITagble
+public class TagPackage : MonoBehaviour, ITagble
 {
     public TagData PackageTagData;
 
