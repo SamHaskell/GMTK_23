4d7e9c987fb7735d0c4a50eb935d9ea8f6f2f9a9
SamHaskell <samuel.haskell@manchester.ac.uk>
2023-07-09 17:57:17 +0100

    scene transition stuff

--- Assets/Project/Scripts/Logic/LoadingScene.cs
+++ Assets/Project/Scripts/Logic/LoadingScene.cs
@@ -6,16 +6,13 @@ using FMODUnity;
 
 public class LoadingScene : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
     
-    // Update is called once per frame
     void Update()
     {
-        // if(AudioManager.instance.GetComponent<StudioBankLoader>() ge)
 
     }
 }
--- Assets/Project/Scripts/Logic/MainMenu.cs
+++ Assets/Project/Scripts/Logic/MainMenu.cs
@@ -5,13 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private string newGameSceneName1;
     public void NewGame()
     {
-        SceneManager.LoadScene(newGameSceneName1, LoadSceneMode.Single);
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
         PlayerPrefs.SetInt("Score", 0);
     }
     public void ToTutorial(){
-        SceneManager.LoadScene(3, LoadSceneMode.Single);
+        SceneManager.LoadScene(4, LoadSceneMode.Single);
     }
 }
