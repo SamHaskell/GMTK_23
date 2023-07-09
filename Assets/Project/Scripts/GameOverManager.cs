ffe1d4d77fec7940372865807ce2df0f312486b3
peanut-weanut <109343736+peanut-weanut@users.noreply.github.com>
2023-07-09 08:47:32 -0700

    Game Over and Main Menu Changes

--- Assets/Project/Scripts/GameOverManager.cs
+++ Assets/Project/Scripts/GameOverManager.cs
@@ -3,26 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameOverManager : MonoBehaviour
 {
     float _reloadTimer;
-    public Text text;
+    public TextMeshProUGUI text;
     public float TimerLimit;
     // Start is called before the first frame update
     void Start()
     {
-        _reloadTimer = 0;
+        // _reloadTimer = 0;
+        text.text = PlayerPrefs.GetInt("Score", 0).ToString();
     }
 
     
     // Update is called once per frame
     void Update()
     {
-        _reloadTimer += Time.deltaTime;
-        if (_reloadTimer >= TimerLimit) // do this for ychai
-        {
+        if(InputManager.anyKeyDown){
             SceneManager.LoadScene("InitializationScene");
         }
+        // _reloadTimer += Time.deltaTime;
+        // if (_reloadTimer >= TimerLimit) // do this for ychai
+        // {
+            // SceneManager.LoadScene("InitializationScene");
+        // }
     }
 }
