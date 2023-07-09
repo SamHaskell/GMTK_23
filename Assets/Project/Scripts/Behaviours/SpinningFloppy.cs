909c6c7beb28fd556fb859b3368b952282c49750
SamHaskell <samuel.haskell@manchester.ac.uk>
2023-07-09 14:36:02 +0100

    added simple movement for floppy

--- Assets/Project/Scripts/Behaviours/SpinningFloppy.cs
+++ Assets/Project/Scripts/Behaviours/SpinningFloppy.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SpinningFloppy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector3 DisplacementAmplitudes;
+    public Vector3 DisplacementFrequencies;
+    private Vector3 _origin;
+    private float _time;
     void Start()
     {
-        
+        _origin = transform.position;
+        _time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        _time += Time.deltaTime;
+        Vector3 displacement = new Vector3(
+            DisplacementAmplitudes.x * Mathf.Sin(_time * DisplacementFrequencies.x),
+            DisplacementAmplitudes.y * Mathf.Sin(_time * DisplacementFrequencies.y),
+            DisplacementAmplitudes.z * Mathf.Sin(_time * DisplacementFrequencies.z)
+        );
+        transform.position = _origin + displacement;
         
     }
 }
