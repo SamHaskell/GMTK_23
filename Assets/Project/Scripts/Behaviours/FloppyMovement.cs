9c5bada5346767e5f4a790a3edd09137bd2a9d0b
SamHaskell <samuel.haskell@manchester.ac.uk>
2023-07-09 14:42:15 +0100

    added random starts

--- Assets/Project/Scripts/Behaviours/FloppyMovement.cs
+++ Assets/Project/Scripts/Behaviours/FloppyMovement.cs
@@ -7,10 +7,15 @@ public class FloppyMovement : MonoBehaviour
     public Vector3 DisplacementAmplitudes;
     public Vector3 DisplacementFrequencies;
     private Vector3 _origin;
+    private Vector3 _timeOffset;
     private float _time;
     void Start()
     {
         _origin = transform.position;
+        _timeOffset = new Vector3(
+            Random.Range(0.0f, 100.0f),
+            Random.Range(0.0f, 100.0f),
+            Random.Range(0.0f, 100.0f));
         _time = 0;
     }
 
@@ -19,9 +24,9 @@ public class FloppyMovement : MonoBehaviour
     {
         _time += Time.deltaTime;
         Vector3 displacement = new Vector3(
-            DisplacementAmplitudes.x * Mathf.Sin(_time * DisplacementFrequencies.x),
-            DisplacementAmplitudes.y * Mathf.Sin(_time * DisplacementFrequencies.y),
-            DisplacementAmplitudes.z * Mathf.Sin(_time * DisplacementFrequencies.z)
+            DisplacementAmplitudes.x * Mathf.Sin((_time + _timeOffset.x) * DisplacementFrequencies.x),
+            DisplacementAmplitudes.y * Mathf.Sin((_time + _timeOffset.y) * DisplacementFrequencies.y),
+            DisplacementAmplitudes.z * Mathf.Sin((_time + _timeOffset.z) * DisplacementFrequencies.z)
         );
         transform.position = _origin + displacement;
         
