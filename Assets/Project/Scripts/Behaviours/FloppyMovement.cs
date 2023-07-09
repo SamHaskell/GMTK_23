dba258377f272fbf21b819e93844d1934a66c2c9
SamHaskell <samuel.haskell@manchester.ac.uk>
2023-07-09 14:36:08 +0100

    movement

---
+++ Assets/Project/Scripts/Behaviours/FloppyMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloppyMovement : MonoBehaviour
+{
+    public Vector3 DisplacementAmplitudes;
+    public Vector3 DisplacementFrequencies;
+    private Vector3 _origin;
+    private float _time;
+    void Start()
+    {
+        _origin = transform.position;
+        _time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time += Time.deltaTime;
+        Vector3 displacement = new Vector3(
+            DisplacementAmplitudes.x * Mathf.Sin(_time * DisplacementFrequencies.x),
+            DisplacementAmplitudes.y * Mathf.Sin(_time * DisplacementFrequencies.y),
+            DisplacementAmplitudes.z * Mathf.Sin(_time * DisplacementFrequencies.z)
+        );
+        transform.position = _origin + displacement;
+        
+    }
+}
